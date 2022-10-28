d6f1c914127691615137016103e9aa772541a62c
Eric Carlisle <eric@ericcarlisle.com>
2022-10-28 09:25:26 -0400

    Add in application Sass.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import FileDownload from "./FileDownload";
 import FileData from "./data/FileData";
+import "./App.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
